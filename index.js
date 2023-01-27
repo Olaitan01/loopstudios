73334e3d63d342ab1160110c7ff997e5d880d2f1
Olaitan01 <109383139+Olaitan01@users.noreply.github.com>
2023-01-27 01:29:03 +0100

    updated js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const closeIcon= document.querySelector(".closeIcon");
 hamburger.addEventListener("click",()=>{
     if(hamburger.classList.contains("active")){
         hamburger.classList.toggle("active")
-        headerNav.style.display="block"
-        menuIcon.style.display="none"
-        closeIcon.style.display="block"
-    }else{
-        hamburger.classList.toggle("active")
         headerNav.style.display="none"
         menuIcon.style.display="block"
         closeIcon.style.display="none"
+    }else{
+        hamburger.classList.toggle("active")
+        headerNav.style.display="block"
+        menuIcon.style.display="none"
+        closeIcon.style.display="block"
     }
 })
